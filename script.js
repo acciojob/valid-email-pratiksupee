566c54c493075440609df9ceb8bf8c98458ce5a1
pratiksupee <135314162+pratiksupee@users.noreply.github.com>
2026-01-22 23:23:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	if (!str) {
+    return false;
+  }
+  const emailRegex = /^[A-Za-z0-9]+([.-][A-Za-z0-9]+)*@[A-Za-z0-9]+([.-][A-Za-z0-9]+)*\.[A-Za-z]{2,3}(\.[A-Za-z]{2,3})?$/;
+  return emailRegex.test(str);
 }
 
 // Do not change the code below.
